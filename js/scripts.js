a99a1fdb5e04e289926da6b71bbf5fd397cabbd9
Jared schroeder <schroedertheory@gmail.com>
2024-06-08 20:45:59 +0200

    0.1

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,10 @@
-console.log('Welcome to My Blog');
+// js/scripts.js
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const toggleButton = document.getElementById('js-navbar-toggle');
+    const menu = document.getElementById('js-menu');
+
+    toggleButton.addEventListener('click', () => {
+        menu.classList.toggle('show');
+    });
+});
